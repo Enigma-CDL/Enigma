929337169edfc9cc1a42d0d077f315cd1aae0013
seek2fly <seek2fly@users.noreply.github.com>
2020-07-22 12:45:54 -0400

    Flight Segment Loader and Graphing helper

---
+++ flightHelper.py
@@ -0,0 +1,185 @@
+
+
+HomeBaseWeightOffset =  4
+HomeBases = { "MEM" : 1 }
+
+NotHomeBaseWeight = makeBaseWeight(len(HomeBases)+1) 
+# NotHomeBaseWeight = makeBaseWeight(1) 
+
+
+# Definitions required to support dependant Lagrange settings (see further below)
+# Base Weight calculation
+
+def makeBaseWeight(id):
+    # Weight is calculated as: 10 ** ( id + offset +1 )
+    #
+    return( 10 ** ( id + HomeBaseWeightOffset + 1))
+
+# Home Base definitions
+#
+# Each valid home base is defined to be assigned its own Weight
+# Weight is added on the first node and subtracted on the last node. If bases match, net 0 effect, no penalty
+#
+#
+# Variations: Try LCA alone, ATH alone or both LCA and ATH
+#
+# 
+
+# Segment Class
+
+class Segment:
+
+    def getUarrtime(self):
+        return ((self.getUdeptime() + self.ft))
+                
+    def getUdeptime(self):
+        return ((self.depday - 1) * 1440 + self.deptime)
+    
+    def setT(self, T):
+        self.T = T
+        self.UT1 = self.getUdeptime()
+        self.UT2 = T - ( self.getUdeptime() + self.ft)
+        #print( self.UT1, self.UT2, T, self.getUdeptime, self.getUarrtime)
+        
+    def getUT(self):
+        return( self.T - self.ft)
+
+    def getUT1(self):
+        return( self.UT1)
+
+    def getUT2(self):
+        return( self.UT2)
+    
+    def setCI(self,ci):
+        self.ci = ci
+        
+    def setCO(self,co):
+        self.co = co
+        
+    def getCI(self):
+        return self.ci
+        
+    def getCO(self):
+        return self.co
+        
+    def __init__(self, id, lab, dep, arr, deptime, arrtime, depday, arrday):
+        self.id = id
+        self.lab = lab
+        self.dep = dep
+        self.arr = arr
+        self.deptime = deptime
+        self.arrtime = arrtime
+        self.depday = depday
+        self.arrday = arrday
+        self.ft = (arrday-1) * 1440 + arrtime - ( (depday-1) * 1440 + deptime )
+        self.UT1 = 0
+        self.UT2 = 0
+        self.T = 0
+        self.ci = 0
+        self.co = 0
+        
+        # Establish the weights associated with departure and arrival airport
+        # 
+        # Only apply a weight for base airports
+        #
+        
+        self.DepBaseWgt = 0 #NotHomeBaseWeight
+        self.ArrBaseWgt = 0 #NotHomeBaseWeight
+        
+        if ( self.dep in HomeBases):
+            self.DepBaseWgt = makeBaseWeight(HomeBases[dep])
+            
+        if ( self.arr in HomeBases):
+            self.ArrBaseWgt = makeBaseWeight(HomeBases[arr])
+        
+        #  Not required ... yet. The idea of carrying forward a weight from node to node may still be useful
+        # self.TransitionBaseWeight = self.DepBaseWgt - self.ArrBaseWgt
+
+# Create Start objects for adding as special edges
+
+# Todo: Rename a "start" to a "break". 
+
+class Start:
+
+    def __init__(self, id, lab):
+        self.id = id
+        self.lab = lab
+
+class Node:
+
+    def isStart(self):
+        return isinstance(self.obj,Start)
+    
+    def isSegment(self):
+        return isinstance(self.obj,Segment)
+    
+    def __init__(self, obj ):
+        if ( isinstance(obj,Segment)):
+            self.id = obj.id
+            self.lab = obj.lab
+            self.obj = obj
+            #print("Node %d is %s" % (self.id, self.lab))
+        elif ( isinstance(obj,Start)):
+            self.id = obj.id
+            self.lab = obj.lab
+            self.obj = obj
+            #print("We have Start State %d" % (self.id))
+        else:
+            raise Exception("Error on type. Got %s" % (type(obj)))
+
+# print("Testing Node class")
+# n1 = Node(Segment(1, "101", "LCA", "ATH", 600, 695, 1, 1))
+# n2 = Node(State(N*N,"start"))
+# print( n1.isStart() )
+# print( n2.isStart() )
+# n3 = Node(10)    # raises exception as expected
+        
+
+    
+    
+    
+# Connection Weight
+#
+# Includes: Time gap, origin
+#
+
+class ConnectWeight:
+
+    # 1) Connection Time gap. If negative make it zero. Zero gaps will be excluded from edges.  
+    # 2) HomeBase switch weight
+    # 3) Matching Airport connection: If does not match, assign NegativeWeight
+    #
+
+    def __init__ (self, seg1, seg2):
+        
+        #NegativeWeight = 10**(HomeBaseWeightOffset+1)  # TODO: Parametrize this definition
+
+        # Include FT of nodes to allow for constraints on maximum flight time
+        #
+        
+        self.ft = seg1.ft + seg2.ft
+        
+        # Include the time gap for the connection
+
+        self.gap = gap(seg1,seg2)
+        
+        if ( self.gap < 0 ): 
+            self.gap = 0 # abs(NegativeWeight + self.gap)
+
+        # Connect Weight for Airports:
+        #
+        # bi,j: is the difference between the seg2.arr weight and seg1.dep weight
+        # This is the TransitionalBaseWeight for the connection.
+        #
+        # TODO: Decide on abs value or signed
+                    
+        self.TransitionBaseWeight = seg2.ArrBaseWgt - seg1.DepBaseWgt
+        
+        # TODO: Decide how to deal with a gap when not connecting at the same airport
+        
+        if ( seg2.dep != seg1.arr ):
+            self.gap = 0 # NegativeWeight;
+        #print(self.__dict__)
+        
+def gap(seg1,seg2):
+	return( seg2.depday * 1440 + seg2.deptime - (seg1.depday * 1440 + seg1.deptime + seg1.ft ))

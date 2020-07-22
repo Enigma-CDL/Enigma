924f5394e5bc103a42557043f70240bc199b1094
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-07-22 23:29:38 +0100

    fix adj mat test

--- qGAN/tests/test_qgan.py
+++ qGAN/tests/test_qgan.py
@@ -1,9 +1,22 @@
 import unittest
+from qGAN.qGAN import qGAN
+import numpy as np
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_time_to_adj(self):
+        to = np.array([[0,1,0,0],
+                       [1, 0, 0, 0],
+                       [0,0,0,1],
+                       [0,0,1,0]])
+        # array([1, 0, 3, 2])
+        truth = np.array([[0,0,0,1],
+                         [1,0,0,0],
+                         [0,0,0,0],
+                         [0,0,1,0]])
+        test = qGAN.time_ordered_to_adjacency(to)
+        np.testing.assert_array_equal(test, truth)
 
 
 if __name__ == '__main__':

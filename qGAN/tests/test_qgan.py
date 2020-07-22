f43c8928e657bbf8e0ee771378c7e89303960d12
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-07-23 00:13:28 +0100

    test qgan

--- qGAN/tests/test_qgan.py
+++ qGAN/tests/test_qgan.py
@@ -1,9 +1,9 @@
 import unittest
-from qGAN.qGAN import qGAN
+from qGAN.qGAN import qGAN, create_qGAN
 import numpy as np
 
 
-class MyTestCase(unittest.TestCase):
+class testqGAN(unittest.TestCase):
 
     def test_time_to_adj(self):
         to = np.array([[0,1,0,0],
@@ -17,6 +17,18 @@ class MyTestCase(unittest.TestCase):
                          [0,0,1,0]])
         test = qGAN.time_ordered_to_adjacency(to)
         np.testing.assert_array_equal(test, truth)
+
+    def test_with_one_mat(self):
+        adj_mat = np.array([[0, 5, 1, 0], [7, 0, 1, 2], [1, 4, 0, 3], [1, 2, 1, 0]])
+        x_samples = [np.array([[0,0,0,1],
+                             [1,0,0,0],
+                             [0,0,0,0],
+                             [0,0,1,0]]),
+                          np.array([[0,0,0,1],
+                             [1,0,0,0],
+                             [0,0,0,0],
+                             [0,0,1,0]])]
+        create_qGAN(adj_mat, x_samples)
 
 
 if __name__ == '__main__':

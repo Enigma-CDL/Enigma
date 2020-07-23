e965c5e4423f4333649138d41e4579e289db12ac
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-07-23 11:34:16 +0100

    add CV functionality

--- qGAN/qGAN.py
+++ qGAN/qGAN.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import pennylane as qml
 import numpy as np
 import os
+from recirq.optimize.mgd import model_gradient_descent
 
 from typing import Union, List, Tuple
 
@@ -10,8 +11,21 @@ class qGAN:
 
     def __init__(self, n_qubits, gen_dev: str = 'default.qubit.tf',  disc_dev: str = 'default.qubit.tf'):
         self.n_qubits = n_qubits
-        self.gen_dev = qml.device(gen_dev, wires=n_qubits)
-        self.disc_dev = qml.device(disc_dev, wires=n_qubits)
+        try:
+            self.gen_dev = qml.device(gen_dev, wires=n_qubits)
+            self.disc_dev = qml.device(disc_dev, wires=n_qubits)
+        except TypeError:
+            self.gen_dev = qml.device(gen_dev, wires=n_qubits, cutoff_dim=n_qubits)
+            self.disc_dev = qml.device(disc_dev, wires=n_qubits, cutoff_dim=n_qubits)
+        if 'GaussianState' not in self.gen_dev.operations:
+            self.create_real, self.generator, self.discriminator = self.create_real_qubit, self.generator_qubit, \
+                                                               self.discriminator_qubit
+            self.qubit = True
+        else:
+            self.create_real, self.generator, self.discriminator = self.create_real_qmode, self.generator_qmode,\
+                                                                   self.discriminator_qmode
+            self.qubit = False
+
 
     @staticmethod
     def tsp_cost(adjacency_matrix: np.ndarray, solution_vector: np.ndarray):
@@ -65,7 +79,7 @@ class qGAN:
             last = index
         return adjacency
 
-    def create_real(self, adjacency_matrix: np.ndarray):
+    def create_real_qubit(self, adjacency_matrix: np.ndarray):
         if np.sum(adjacency_matrix.shape) > self.n_qubits:
             raise ValueError('The adjacency matrix provided is too large')
         adj_vec = np.reshape(adjacency_matrix, self.n_qubits)
@@ -73,7 +87,15 @@ class qGAN:
             if connection:
                 qml.RX((np.pi), wires=i)
 
-    def generator(self, weights: List[Tuple[float]], **kwargs):
+    def create_real_qmode(self, adjacency_matrix: np.ndarray):
+        if np.sum(adjacency_matrix.shape) > self.n_qubits:
+            raise ValueError('The adjacency matrix provided is too large')
+        adj_vec = np.reshape(adjacency_matrix, self.n_qubits)
+        for i, connection in enumerate(adj_vec):
+            if connection:
+                qml.FockState(1, wires=i)
+
+    def generator_qubit(self, weights: List[Tuple[float]], **kwargs):
         qb_list = list(range(self.n_qubits))
         for i, l in enumerate(range(int(self.n_qubits / 2), 0, -1)):
             entanglers = qb_list[l:l-i]
@@ -83,17 +105,33 @@ class qGAN:
         for qb in range(self.n_qubits):
             qml.RY(weights[qb], wires=qb)
 
-    def discriminator(self, weights: List[Tuple[float]], **kwargs):
+    def generator_qmode(self, weights: List[Tuple[float]], **kwargs):
+        qb_list = list(range(self.n_qubits))
+        for i, l in enumerate(range(int(self.n_qubits / 2), 0, -1)):
+            entanglers = qb_list[l:l-i]
+            for i, (control, target) in enumerate(zip(entanglers[::-1], entanglers[::-1][1:])):
+                qml.ControlledAddition(weights[self.n_qubits + i], wires=[control, target])
+        for qb in range(self.n_qubits):
+            qml.SqueezedState(weights[qb], weights[qb + self.n_qubits], wires=qb)
+
+    def discriminator_qubit(self, weights: List[Tuple[float]], **kwargs):
         for qb in range(self.n_qubits):
             qml.RZ(weights[qb], wires=qb)
             qml.RX(weights[qb + self.n_qubits], wires=qb)
         qb_list = list(range(self.n_qubits))
         for i, (control, target) in enumerate(zip(qb_list[::-1], qb_list[::-1][1:])):
-            #qml.CNOT(wires=[control, target])
             qGAN.iSWAP(weights[2 * self.n_qubits + i], wires=[control, target])
 
+    def discriminator_qmode(self, weights: List[Tuple[float]], **kwargs):
+        for qb in range(self.n_qubits):
+            qml.SqueezedState(weights[qb], weights[self.n_qubits + qb], wires=qb)
+        qb_list = list(range(self.n_qubits))
+        for i, (control, target) in enumerate(zip(qb_list[::-1], qb_list[::-1][1:])):
+            qml.ControlledAddition(weights[2* self.n_qubits + i], wires=[control, target])
 
-def create_qGAN(adjacency_matrix: np.ndarray, x_samples: List[np.ndarray], epochs: int = 15, lr: float = 0.02):
+
+def train_qGAN(adjacency_matrix: np.ndarray, x_samples: List[np.ndarray], epochs: int = 15, lr: float = 0.02,
+               gen_dev: str = 'qiskit.aer', disc_dev: str = 'qiskit.aer'):
     """
     Given an adjacency matrix and some sampled solved problems, this function will build the correct size qGAN and
      train it to attempt to generate novel data.
@@ -105,7 +143,8 @@ def create_qGAN(adjacency_matrix: np.ndarray, x_samples: List[np.ndarray], epoch
     """
     n_cities = adjacency_matrix.shape[0]
     n_qubits = adjacency_matrix.shape[0] ** 2
-    qgan = qGAN(n_qubits)
+    qgan = qGAN(n_qubits, gen_dev, disc_dev)
+    observable = qml.PauliZ if qgan.qubit else qml.NumberOperator
 
     @qml.qnode(qgan.disc_dev, interface='tf')
     def real_disc_circuits(adjaceny_matrix: np.ndarray, disc_weights):
@@ -113,20 +152,20 @@ def create_qGAN(adjacency_matrix: np.ndarray, x_samples: List[np.ndarray], epoch
         Probability of the data being real is given by measurement of first qubit"""
         qgan.create_real(adjacency_matrix=adjaceny_matrix)
         qgan.discriminator(disc_weights)
-        return qml.expval(qml.PauliZ(0))
+        return qml.expval(observable(0))
 
     @qml.qnode(qgan.gen_dev, interface='tf')
     def gen_disc_circuits(gen_weights, disc_weights):
         """Builds the discrimination circuit with generated data."""
         qgan.generator(gen_weights)
         qgan.discriminator(disc_weights)
-        return qml.expval(qml.PauliZ(0))
+        return qml.expval(observable(0))
 
-    @qml.qnode(qgan.gen_dev, interface='tf')
+    @qml.qnode(qgan.gen_dev, interface='tf', shots=1)
     def generate_sample(gen_weights):
         """Just samples from the generated circuit"""
         qgan.generator(gen_weights)
-        return [qml.expval(qml.PauliZ(x)) for x in range(n_qubits)]
+        return [qml.sample(observable(x)) for x in range(n_qubits)]
 
     def real_true(sample_solution, disc_weights):
         """Probability of measuring true when given real data"""
@@ -186,14 +225,23 @@ def create_qGAN(adjacency_matrix: np.ndarray, x_samples: List[np.ndarray], epoch
         optimiser.apply_gradients(zip(grads, [gen_weights]))
         return gen_loss
 
+    def sample(n_samples, gen_weights):
+        samples = []
+        for _ in range(n_samples):
+            s = generate_sample(gen_weights)
+            samples.append(s)
+            print('Sampled output: {}'.format(s))
+        return samples
+
     def training(x_train):
         init_gen = np.random.normal(size=(2* n_qubits, ))
         init_disc = np.random.normal(size=((3 * n_qubits) - 1, ))
         gen_weights = tf.Variable(init_gen)
         disc_weights = tf.Variable(init_disc)
 
-        checkpoint_dir = './checkpoints/'
-        chkpt_prefix = os.path.join(checkpoint_dir, 'ckpt')
+        checkpoint_dir = './qgan_checkpoints/'
+        chkpt_prefix = os.path.join(checkpoint_dir, 'qubit_{}_ckpt'.format(qgan.qubit))
+
         optimiser = tf.optimizers.SGD(lr)
         chkpt = tf.train.Checkpoint(optimizer=optimiser, disc_weights=disc_weights,
                                     gen_weights=gen_weights)
@@ -206,5 +254,8 @@ def create_qGAN(adjacency_matrix: np.ndarray, x_samples: List[np.ndarray], epoch
                 sample = (np.round(generate_sample(gen_weights)).reshape((n_cities, n_cities)) + 1) / 2
                 print('Generated sample:\n{}'.format(np.round(sample)))
                 chkpt.save(file_prefix=chkpt_prefix)
+        return gen_weights, disc_weights
 
-    training(x_samples)
+    gen_weights, _ = training(x_samples)
+    samples = sample(10, gen_weights)
+    return samples

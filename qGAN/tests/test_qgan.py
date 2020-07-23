f33cdc376a0e75f3c9acdc7137b9c231331f6b76
Andrew Patterson <andrew.pattersonap24@googlemail.com>
2020-07-23 11:35:00 +0100

    test CV qGAN

--- qGAN/tests/test_qgan.py
+++ qGAN/tests/test_qgan.py
@@ -1,5 +1,5 @@
 import unittest
-from qGAN.qGAN import qGAN, create_qGAN
+from qGAN.qGAN import qGAN, train_qGAN
 import numpy as np
 
 
@@ -21,7 +21,7 @@ class testqGAN(unittest.TestCase):
     def test_with_one_mat(self):
         adj_mat = np.array([[0, 5], [1, 2]])
         x_samples = [np.array([[0,1], [0,0]]), np.array([[0,1], [0,0]])]
-        create_qGAN(adj_mat, x_samples, 50)
+        train_qGAN(adj_mat, x_samples, 5, gen_dev='qiskit.aer', disc_dev='qiskit.aer')
 
 
 if __name__ == '__main__':
